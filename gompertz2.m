fd43f1103ff5aaf685a46f20933d11f46d5f8c25
timothy rudge <tjr34@timothys-MacBook-Pro.local>
2015-02-23 18:40:20 +0000

    Corrected comment

--- gompertz2.m
+++ gompertz2.m
@@ -1,6 +1,6 @@
 function y = gompertz2(p, t);
 
-% p = log( [K, lambda, mu] )
+% p = [log(K), mu] )
 K = exp(p(1));
 lambda = 0; % With low initial dilution lag phase is too short to estimate
 mu = exp(p(2));
